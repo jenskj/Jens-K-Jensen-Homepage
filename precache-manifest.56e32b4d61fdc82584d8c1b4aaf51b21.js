f488914e9ea8a6d115bd0e2577bca4a04ee3bee8
jenskj <jens_kj@hotmail.com>
2018-12-11 13:10:29 +0100

    Updates

---
+++ precache-manifest.56e32b4d61fdc82584d8c1b4aaf51b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "473b3032fdc63b32a54ddf2c2276146b",
+    "url": "/PublishedHomepage/static/media/historicalTimeline.473b3032.png"
+  },
+  {
+    "revision": "53488488a1e4ca8881ef804869276532",
+    "url": "/PublishedHomepage/static/media/OCEAN.53488488.png"
+  },
+  {
+    "revision": "4c1d7df8835ea6dfbd2b",
+    "url": "/PublishedHomepage/static/js/runtime~main.4c1d7df8.js"
+  },
+  {
+    "revision": "5dd8ef0ce9481f0b0795",
+    "url": "/PublishedHomepage/static/js/main.5dd8ef0c.chunk.js"
+  },
+  {
+    "revision": "023280a153e8704258dc",
+    "url": "/PublishedHomepage/static/js/1.023280a1.chunk.js"
+  },
+  {
+    "revision": "5dd8ef0ce9481f0b0795",
+    "url": "/PublishedHomepage/static/css/main.bab8f26a.chunk.css"
+  },
+  {
+    "revision": "7064989592f45f25d8b8ac7cc59fefe5",
+    "url": "/PublishedHomepage/index.html"
+  }
+];
