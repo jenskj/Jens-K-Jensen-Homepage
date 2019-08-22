b699196172d12c3a6c060682937fdfb78f39212e
jenskj <jens_kj@hotmail.com>
2019-08-22 22:49:42 +0200

    Updates

---
+++ precache-manifest.3a931aaa509ee7ddedfe1713e480c703.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77af3d145b5c35f8d9703c5c2170bf6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5ea4098f3a567f5038c",
+    "url": "/static/css/main.8f473849.chunk.css"
+  },
+  {
+    "revision": "eac646cbd87dbf2427f5",
+    "url": "/static/js/2.6aa58888.chunk.js"
+  },
+  {
+    "revision": "e5ea4098f3a567f5038c",
+    "url": "/static/js/main.11e887d6.chunk.js"
+  },
+  {
+    "revision": "b0fae3ec06aa9957cb3d",
+    "url": "/static/js/runtime~main.277cae15.js"
+  },
+  {
+    "revision": "473b3032fdc63b32a54ddf2c2276146b",
+    "url": "/static/media/a-timeline.473b3032.png"
+  },
+  {
+    "revision": "1da93f8985064b82640552f6ac6a1b59",
+    "url": "/static/media/b-AnimalSounds.1da93f89.png"
+  },
+  {
+    "revision": "213d1270866892c828d67d9ff3d6ddea",
+    "url": "/static/media/c-RecipeApp.213d1270.png"
+  },
+  {
+    "revision": "53488488a1e4ca8881ef804869276532",
+    "url": "/static/media/d-OCEAN.53488488.png"
+  },
+  {
+    "revision": "687847d53f65b3f997c55d1f1854baf7",
+    "url": "/static/media/e-TypeaheadSearchbar.687847d5.png"
+  }
+]);
